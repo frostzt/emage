77d387dacc45dc981c38f5e9cb5f128df27f775c
frostzt <aidenfrostbite@gmail.com>
2021-04-11 15:58:53 +0530

    Configuring react

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
+// Setup server
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
