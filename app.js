55a349dd215596f6ce58978b034bd78ddd1dd856
frostzt <aidenfrostbite@gmail.com>
2021-04-13 20:11:36 +0530

    Working on API and creating image upload and manipulation routes and controllers

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-const express = require("express");
-const morgan = require("morgan");
+// Core modules
+const cors = require("cors");
 const sharp = require("sharp");
+const morgan = require("morgan");
+const xss = require("xss-clean");
+const helmet = require("helmet");
+const express = require("express");
+const compression = require("compression");
 
 // Create the express instance
 const app = express();
+
+// Implement CORS
+app.use(cors());
+
+// Implement security headers
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", "https:", "http:", "data:", "ws:"],
+      baseUri: ["'self'"],
+      fontSrc: ["'self'", "https:", "http:", "data:"],
+      scriptSrc: ["'self'", "https:", "http:", "blob:"],
+      styleSrc: ["'self'", "unsafe-inline", "https:", "http:"],
+    },
+  })
+);
+
+// Data sanitization and compression
+app.use(xss());
+app.use(compression());
 
 // Morgan logging if on dev env
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Routes
 
 // Body parser
 app.use(express.json({ limit: "10kb" }));
