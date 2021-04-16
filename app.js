083b761b7176f60f9ed918b7abc3de07c1708ded
frostzt <aidenfrostbite@gmail.com>
2021-04-16 18:24:20 +0530

    Prepared for deployment to heroku

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.use(cors());
 // Serve static resources
 app.use(express.static(path.join(__dirname, "public")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Implement security headers
 app.use(helmet());
 app.use(
